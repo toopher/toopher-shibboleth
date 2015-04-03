81dd44c07d2670d03e539f792363f24b6387cfd4
Drew Shafer <ashafer@salesforce.com>
2015-04-03 16:14:33 -0500

    code changes for latest toopher-java code

--- src/main/java/com/toopher/shibboleth/mcb/ToopherLoginSubmodule.java
+++ src/main/java/com/toopher/shibboleth/mcb/ToopherLoginSubmodule.java
@@ -24,6 +24,9 @@ THE SOFTWARE.
 
 package com.toopher.shibboleth.mcb;
 
+import com.toopher.AuthenticationRequest;
+import com.toopher.RequestError;
+import com.toopher.ToopherClientError;
 import com.toopher.ToopherIframe;
 import edu.internet2.middleware.assurance.mcb.authn.provider.MCBAttributeResolver;
 import edu.internet2.middleware.assurance.mcb.authn.provider.MCBLoginServlet;
@@ -35,11 +38,16 @@ import edu.internet2.middleware.shibboleth.common.attribute.BaseAttribute;
 import edu.internet2.middleware.shibboleth.idp.authn.AuthenticationException;
 import edu.internet2.middleware.shibboleth.idp.authn.LoginHandler;
 import java.math.BigInteger;
+import java.net.URISyntaxException;
 import java.security.SecureRandom;
-import java.util.Map;
+import java.util.*;
 import javax.security.auth.login.LoginException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.velocity.VelocityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -62,7 +70,7 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 	private boolean toopherManual;
 	private boolean toopherChallenge;
 	
-	private ToopherIframe t;
+	private ToopherIframe toopherIframe;
 	
 	private static final SecureRandom secureRandom = new SecureRandom();
 	
@@ -105,13 +113,13 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 		this.loginPage = loginPage;
 		
 		if(apiUrl == null || apiUrl.equalsIgnoreCase("")){
-			t = new ToopherIframe(consumerKey, consumerSecret);
+			toopherIframe = new ToopherIframe(consumerKey, consumerSecret);
 		}else{
 			log.info("Using Toopher On-Prem located at {}", apiUrl);
-			t = new ToopherIframe(consumerKey, consumerSecret, apiUrl);
+			toopherIframe = new ToopherIframe(consumerKey, consumerSecret, apiUrl);
 		}
 		
-		log.debug("Config {}: consumer key:{}, consumer secret:{}, login page: {}, toopherChallenge: {}, toopherManual: {}, user mail attribute: {}", beanName,key, secret, loginPage, challenge, manual, emailAttribute);
+		log.debug("Config {}: consumer key:{}, consumer secret:{}, login page: {}, toopherChallenge: {}, toopherManual: {}, user mail attribute: {}", beanName, key, secret, loginPage, challenge, manual, emailAttribute);
 	}
 	
 	/**
@@ -147,8 +155,19 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 			log.debug("No service name in Metadata, falling back to EntityID for service name");
 			serviceName = idpui.getEntityID();
 		}
-		
-		String authIframeURL = t.authUri(principal.getName(), (userEmail == null ? "" : userEmail),serviceName, toopherManual, toopherChallenge, requestToken, "performAuthentication", 60);
+
+
+		Map<String, String> iframeParams = new HashMap<String, String>();
+		if (userEmail != null) {
+			iframeParams.put("reset_email", userEmail);
+		}
+		iframeParams.put("action_name", serviceName);
+		iframeParams.put("automation_allowed", toopherManual ? "True" : "False");
+		iframeParams.put("challenge_required", toopherChallenge ? "True" : "False");
+		iframeParams.put("session_token", requestToken);
+
+
+		String authIframeURL = toopherIframe.getAuthenticationUrl(principal.getName(), iframeParams);
 		log.debug("URL for iFrame is {}",authIframeURL);
 		
 		
@@ -161,6 +180,18 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 		return true;
 	}
 
+	private Map<String, String> getUrlEncodedPostbackData(Map<String, String> postbackData) {
+		TreeSet<String> sortedKeys = new TreeSet<String>(postbackData.keySet());
+		List<NameValuePair> sortedData = new ArrayList<NameValuePair>(postbackData.size());
+		for (String key: sortedKeys) {
+			sortedData.add(new BasicNameValuePair(key, postbackData.get(key)));
+		}
+
+		Map<String, String> data =  new HashMap<String, String>();
+		data.put("toopher_iframe_data", URLEncodedUtils.format(sortedData, "UTF-8"));
+		return data;
+	}
+
 	/**
 	 * Process the response from the Login Screen
 	 * @param servlet
@@ -171,60 +202,60 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 	 * @throws LoginException 
 	 */
 	public boolean processLogin(MCBLoginServlet servlet, HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, LoginException {
-		String requestToken = (String)request.getSession().getAttribute("ToopherRequestToken");
-		log.debug("request token is {}",requestToken);
-		
+		String requestToken = (String) request.getSession().getAttribute("ToopherRequestToken");
+		log.debug("request token is {}", requestToken);
+
 		MCBUsernamePrincipal principal = (MCBUsernamePrincipal) request.getSession().getAttribute(LoginHandler.PRINCIPAL_KEY);
-		
-		try{
-			Map<String,String> validatedData = t.validate(request.getParameterMap(), requestToken);
-			
-			if(validatedData.containsKey("error_code")){
-				//Check for API errors
-				String errorCode = validatedData.get("error_code");
-				log.debug("received error code for principal {}: {}",principal.getName(),errorCode);
-				
-				if(errorCode.equals(ToopherIframe.PAIRING_DEACTIVATED)){
-					//user deleted the pairing of their device -- display paring UI
-					log.info("principal {} was pared, but is presently not.  Sending off to pair.", principal.getName());
-					doPairing(servlet,request, response, requestToken, principal);
-					return false;
-				} else if(errorCode.equals(ToopherIframe.USER_OPT_OUT)){
-					//user marked as opt-out in toopher API, grant access
-					log.info("principal {} is in opt-out mode in toopher.  Access allowed.", principal.getName());
-					return true;
-				} else if(errorCode.equals(ToopherIframe.USER_UNKNOWN)){
-					//user has never done Toopher on this server before
-					log.info("principal {} has not been paired before.  Sending off to pair.", principal.getName());
-					doPairing(servlet,request, response, requestToken, principal);
-					return false;
-				}
-			}
-			
-			boolean authPending = validatedData.get("pending").toLowerCase().equals("true");
-			boolean authGranted = validatedData.get("granted").toLowerCase().equals("true");
-			
-			log.debug("authPending is {} && authGranted is {}", authPending, authGranted);
-			
-			boolean result = authGranted && !authPending;
-			
-			if(result){
+
+		try {
+			AuthenticationRequest authenticationRequest = (AuthenticationRequest) toopherIframe.processPostback(getUrlEncodedPostbackData(request.getParameterMap()), requestToken);
+
+			log.debug("authPending is {} && authGranted is {}", authenticationRequest.pending, authenticationRequest.granted);
+
+			boolean result = authenticationRequest.granted && !authenticationRequest.pending;
+
+			if (result) {
 				return result;
-			} else if (authGranted == false){
+			} else if (authenticationRequest.granted == false) {
 				//either authGranted is false or authPending is true
 				log.warn("Principal {} pressed the deny button on their device.", principal.getName());
 				throw new UserInitiatedLoginFailureException();
 			} else {
 				throw new AuthenticationException("Unknown Toopher error");
 			}
-					
-			
-		}catch(ToopherIframe.SignatureValidationError e){
+		} catch (ToopherClientError tce) {
+			//Check for API errors
+			String errorCode = Integer.toString(tce.getToopherErrorCode());
+			log.debug("received error code for principal {}: {}", principal.getName(), tce.getToopherErrorCode());
+
+			if (errorCode.equals(ToopherIframe.PAIRING_DEACTIVATED)) {
+				//user deleted the pairing of their device -- display paring UI
+				log.info("principal {} was pared, but is presently not.  Sending off to pair.", principal.getName());
+				doPairing(servlet, request, response, requestToken, principal);
+				return false;
+			} else if (errorCode.equals(ToopherIframe.USER_OPT_OUT)) {
+				//user marked as opt-out in toopher API, grant access
+				log.info("principal {} is in opt-out mode in toopher.  Access allowed.", principal.getName());
+				return true;
+			} else if (errorCode.equals(ToopherIframe.USER_UNKNOWN)) {
+				//user has never done Toopher on this server before
+				log.info("principal {} has not been paired before.  Sending off to pair.", principal.getName());
+				doPairing(servlet, request, response, requestToken, principal);
+				return false;
+			}
+
+
+		} catch(ToopherIframe.SignatureValidationError e){
 			//signature was invalid
 			log.warn("Signature was invalid on data returned from Toopher: {}", e.getMessage());
 			principal.setFailedLogin("unable to verify signature");
 			return false;
+		} catch (Exception e) {
+			log.warn("RequestError: {0}", e.getMessage());
+			principal.setFailedLogin("Error when verifying postback: " + e.getMessage());
 		}
+
+		return false; // fail closed
 	}
 
 	public void init() {
@@ -244,8 +275,10 @@ public class ToopherLoginSubmodule implements MCBSubmodule{
 	 * 
 	 */
 	private void doPairing(MCBLoginServlet servlet, HttpServletRequest request, HttpServletResponse response,String requestToken, MCBUsernamePrincipal principal) throws AuthenticationException {
+		Map<String, String> iframeParams = new HashMap<String, String>();
+		iframeParams.put("reset_email", getUserEmail(request, response, servlet, principal));
 		VelocityContext vCtx = new VelocityContext();
-		vCtx.put("iframeRequestURL",t.pairUri(principal.getName(), getUserEmail(request, response, servlet, principal)));
+		vCtx.put("iframeRequestURL", toopherIframe.getUserManagementUrl(principal.getName(), iframeParams));
 		
 		log.debug("Displaying Velocity Toopher template [{}]",loginPage);
 		
